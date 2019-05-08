d347e14e980a276dab15cdc660241065ecdeaa17
theochampion <contact.theochampion@gmail.com>
2019-05-08 18:09:49 +1000

    :art: Convert vehicle to ES6 class

--- src/vehicle.js
+++ src/vehicle.js
@@ -1,4 +1,5 @@
 import { Body, Bodies, Composite, Constraint } from "matter-js";
+import { tsConstructorType } from "@babel/types";
 
 /**
  * Creates a composite with simple vehicle setup of bodies and constraints.
@@ -10,54 +11,55 @@ import { Body, Bodies, Composite, Constraint } from "matter-js";
  * @param {number} wheelSize
  * @return {composite} A new composite vehicle body
  */
-const createVehicle = (xx, yy, height, width) => {
-	const wheelToBodyRatio = 2.3;
-	const group = Body.nextGroup(true),
-		wheelXOffset = 0,
-		wheelYOffset = height / 2;
+export default class vehicle {
+	constructor(xx, yy, height, width) {
+		const wheelToBodyRatio = 2.3;
+		const group = Body.nextGroup(true),
+			wheelXOffset = 0,
+			wheelYOffset = height / 2;
 
-	const vehicle = Composite.create({ label: "vehicle" });
-	const body = Bodies.rectangle(
-		xx,
-		yy - width / wheelToBodyRatio,
-		width,
-		height,
-		{
-			collisionFilter: {
-				group: group
-			},
-			chamfer: {
-				radius: 10
-			},
-			density: 0.0002
-		}
-	);
+		const body = Bodies.rectangle(
+			xx,
+			yy - width / wheelToBodyRatio,
+			width,
+			height,
+			{
+				collisionFilter: {
+					group: group
+				},
+				chamfer: {
+					radius: 10
+				},
+				density: 0.0002,
+				label: "vehicle_body"
+			}
+		);
 
-	const wheelA = Bodies.circle(
-		xx + wheelXOffset,
-		yy + wheelYOffset,
-		width / wheelToBodyRatio,
-		{
-			collisionFilter: {
-				group: group
-			},
-			friction: 0.8
-		}
-	);
+		const wheelA = Bodies.circle(
+			xx + wheelXOffset,
+			yy + wheelYOffset,
+			width / wheelToBodyRatio,
+			{
+				collisionFilter: {
+					group: group
+				},
+				friction: 0.8,
+				label: "vehicle_wheel"
+			}
+		);
 
-	const axelA = Constraint.create({
-		bodyB: body,
-		pointB: { x: wheelXOffset, y: wheelYOffset },
-		bodyA: wheelA,
-		stiffness: 1,
-		length: 0
-	});
+		const axelA = Constraint.create({
+			bodyB: body,
+			pointB: { x: wheelXOffset, y: wheelYOffset },
+			bodyA: wheelA,
+			stiffness: 1,
+			length: 0
+		});
 
-	Composite.addBody(vehicle, body);
-	Composite.addBody(vehicle, wheelA);
-	Composite.addConstraint(vehicle, axelA);
-
-	return vehicle;
-};
-
-export default createVehicle;
+		this.composite = Composite.create({ label: "vehicle" });
+		Composite.addBody(this.composite, body);
+		Composite.addBody(this.composite, wheelA);
+		Composite.addConstraint(this.composite, axelA);
+		this.collidableBodyId = body.id;
+	}
+}
