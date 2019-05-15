1aded0d0df79b54606dd81e7cbab70ae9ed72ed2
theochampion <contact.theochampion@gmail.com>
2019-05-15 13:35:18 +1000

    :art: [normalize_env] Fixed scaling of scene

--- src/simulator/scene.js
+++ src/simulator/scene.js
@@ -2,39 +2,46 @@ import { Composite, Body, Bodies } from "matter-js";
 
 export default class Scene {
 	constructor(width, height) {
-		const rightWall = Bodies.rectangle(width + 1, height / 2, 2, height, {
+		const wallOptions = {
 			isSensor: true,
 			isStatic: true
-		});
-		const leftWall = Bodies.rectangle(-1, height / 2, 2, height, {
-			isSensor: true,
-			isStatic: true
-		});
-		const topWall = Bodies.rectangle(width / 2, 0, width, 2, {
-			isSensor: true,
-			isStatic: true
-		});
-		const bottomWall = Bodies.rectangle(width / 2, height, width, 2, {
-			isSensor: true,
-			isStatic: true
-		});
-		rightWall.render.visible = false;
-		leftWall.render.visible = false;
-		topWall.render.visible = false;
-		bottomWall.render.visible = false;
+		};
+		const rightWall = Bodies.rectangle(
+			width / 2 + height / 2,
+			height / 2,
+			1,
+			height,
+			wallOptions
+		);
+		const leftWall = Bodies.rectangle(
+			width / 2 - height / 2,
+			height / 2,
+			1,
+			height,
+			wallOptions
+		);
+		const topWall = Bodies.rectangle(width / 2, 0, height, 1, wallOptions);
+		const bottomWall = Bodies.rectangle(
+			width / 2,
+			height,
+			height,
+			1,
+			wallOptions
+		);
+		// rightWall.render.visible = false;
+		// leftWall.render.visible = false;
+		// topWall.render.visible = false;
+		// bottomWall.render.visible = false;
 
 		this.sineStep = 0.0;
 		this.rotationFactor = 0.0;
 
 		this.composite = Composite.create({ label: "scene" });
-		Composite.addBody(this.composite, leftWall);
-		Composite.addBody(this.composite, rightWall);
-		Composite.addBody(this.composite, topWall);
-		Composite.addBody(this.composite, bottomWall);
+		Composite.add(this.composite, [leftWall, rightWall, topWall, bottomWall]);
 		this.groundWheel = Bodies.circle(
 			width / 2,
-			height * 1.8,
-			height,
+			height * 1.3,
+			height / 2,
 			{
 				isStatic: true,
 				friction: 0.5
