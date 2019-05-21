369cbbf33ab9990742911587fbed908216596c1e
theochampion <contact.theochampion@gmail.com>
2019-05-21 19:05:36 +1000

    :art: [scene] Modify scene mechanic

--- src/simulator/scene.js
+++ src/simulator/scene.js
@@ -2,6 +2,8 @@ import { Composite, Body, Bodies } from "matter-js";
 
 export default class Scene {
 	constructor(width, height) {
+		this.width = width;
+		this.height = height;
 		const wallOptions = {
 			isSensor: true,
 			isStatic: true
@@ -36,32 +38,54 @@ export default class Scene {
 		this.sineStep = 0.0;
 		this.rotationFactor = 0.0;
 
-		this.composite = Composite.create({ label: "scene" });
-		Composite.add(this.composite, [leftWall, rightWall, topWall, bottomWall]);
-		this.groundWheel = Bodies.circle(
-			width / 2,
+		this.walls = Composite.create({ label: "walls" });
+		Composite.add(this.walls, [leftWall, rightWall, topWall, bottomWall]);
+
+		this.wheel = Composite.create({ label: "wheel" });
+		this.mainWheel = Bodies.polygon(width / 2, height * 1.3, 100, height / 2, {
+			isStatic: true,
+			friction: 0.5,
+			chamfer: 500
+		});
+		this.spike = Bodies.polygon(
+			width / 2 + height / 3,
 			height * 1.3,
-			height / 2,
+			100,
+			height / 4,
 			{
 				isStatic: true,
 				friction: 0.5
-			},
-			100
+			}
 		);
-		Body.setAngle(this.groundWheel, -Math.PI / 2);
-		Composite.addBody(this.composite, this.groundWheel);
+		this.spike2 = Bodies.polygon(width / 2, height * 1.3, 4, height / 1.8, {
+			isStatic: true,
+			friction: 0.5,
+			chamfer: 200
+		});
+		Body.setAngle(this.mainWheel, -Math.PI / 2);
+		Composite.add(this.wheel, [this.mainWheel, this.spike, this.spike2]);
+		this.composite = Composite.create({ label: "scene" });
+		Composite.add(this.composite, [this.walls, this.wheel]);
 	}
 
 	reset() {
-		Body.setAngle(this.groundWheel, -Math.PI / 2);
+		//Body.setAngle(this.groundWheel, -Math.PI / 2);
 		this.sineStep = 0;
 		this.rotationFactor = 0;
 	}
 	rotateRandomly(ts) {
-		const stepSpeed = Math.sin(this.sineStep) * this.rotationFactor;
-		Body.rotate(this.groundWheel, stepSpeed);
-		Body.setAngularVelocity(this.groundWheel, stepSpeed);
+		const stepSpeed = this.rotationFactor; //Math.sin(this.sineStep) * this.rotationFactor;
+		Composite.rotate(this.wheel, stepSpeed, {
+			x: this.width / 2,
+			y: this.height * 1.3
+		});
+		// Composite.allBodies(this.wheel).map(body => {
+		// 	Body.rotate(body, stepSpeed);
+		// 	//Body.setAngularVelocity(body, stepSpeed);
+		// });
+		//Composite.rotate(this.wheel, stepSpeed);
+		//Body.setAngularVelocity(this.groundWheel, stepSpeed);
 		this.sineStep += 0.023;
-		this.rotationFactor += 0.00001;
+		this.rotationFactor += 0.00003;
 	}
 }
