8c8ec1fba8106b91229753c3af175322f3b9235c
theochampion <contact.theochampion@gmail.com>
2019-05-22 10:40:52 +1000

    :art: Add stat math utils

--- src/utils/math.js
+++ src/utils/math.js
@@ -3,3 +3,5 @@ export const lerp = (number, in_min, in_max, out_min, out_max) => {
 		((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 	);
 };
+
+export const increasePerc = (a, b) => ((a - b) / b) * 100;
