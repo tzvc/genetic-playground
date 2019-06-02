c5367502e7ad35f8b1af6575a1cbbfb4f364dc72
theochampion <contact.theochampion@gmail.com>
2019-06-02 21:45:15 +1000

    :bulb: Documenting genetic config

--- src/genetic/genetic_config.js
+++ src/genetic/genetic_config.js
@@ -2,40 +2,55 @@ import { addLeadingZeros } from "../utils/string";
 
 const bitLength = Number.MAX_SAFE_INTEGER.toString(2).length;
 
+/**
+ * Seed intitial population with random chromosomes
+ */
 export const seed = () => {
 	const PIDParams = [Math.random(), Math.random(), Math.random()];
-	let genome = "";
+	let chromosome = "";
 	PIDParams.forEach(PIDParam => {
-		genome += addLeadingZeros(
+		chromosome += addLeadingZeros(
 			Math.floor(PIDParam * Number.MAX_SAFE_INTEGER).toString(2),
 			bitLength
 		);
 	});
-	return genome;
+	return chromosome;
 };
 
-export const mutate = entity => {
-	const flipIndex = Math.floor(Math.random() * entity.length);
+/**
+ * Perform random bit flip mutation
+ * @param {String} chromosome Individual's chromosome
+ */
+export const mutate = chromosome => {
+	const flipIndex = Math.floor(Math.random() * chromosome.length);
 	function replaceAt(string, index, replace) {
 		return string.substring(0, index) + replace + string.substring(index + 1);
 	}
-	return replaceAt(entity, flipIndex, entity[flipIndex] === "1" ? "0" : "1");
+	return replaceAt(
+		chromosome,
+		flipIndex,
+		chromosome[flipIndex] === "1" ? "0" : "1"
+	);
 };
 
+/**
+ * Perform random two-point crossover
+ * @param {Object} mother Selected mother chromosome
+ * @param {Object} father Selected father chromosome
+ */
 export const crossover = (mother, father) => {
 	// two-point crossover
-	var len = mother.length;
-	var ca = Math.floor(Math.random() * len);
-	var cb = Math.floor(Math.random() * len);
+	let ca = Math.floor(Math.random() * mother.length);
+	let cb = Math.floor(Math.random() * mother.length);
 	if (ca > cb) {
-		var tmp = cb;
+		const tmp = cb;
 		cb = ca;
 		ca = tmp;
 	}
 
-	var son =
+	const son =
 		father.substr(0, ca) + mother.substr(ca, cb - ca) + father.substr(cb);
-	var daughter =
+	const daughter =
 		mother.substr(0, ca) + father.substr(ca, cb - ca) + mother.substr(cb);
 
 	return [son, daughter];
