4754726c119e2af9cb6ceed960b90c1d6764d97a
theochampion <contact.theochampion@gmail.com>
2019-06-02 21:45:00 +1000

    :bulb: Documenting selectors

--- src/genetic/selectors.js
+++ src/genetic/selectors.js
@@ -1,16 +1,19 @@
+/**
+ * Individual selectors for crossover and mutation operations
+ */
 export const individualSelectors = {
 	tournament2: (population, optimizer) => {
-		var n = population.length;
-		var a = population[Math.floor(Math.random() * n)];
-		var b = population[Math.floor(Math.random() * n)];
+		const n = population.length;
+		const a = population[Math.floor(Math.random() * n)];
+		const b = population[Math.floor(Math.random() * n)];
 		return optimizer(a.fitness, b.fitness) ? a.genome : b.genome;
 	},
 	tournament3: (population, optimizer) => {
-		var n = population.length;
-		var a = population[Math.floor(Math.random() * n)];
-		var b = population[Math.floor(Math.random() * n)];
-		var c = population[Math.floor(Math.random() * n)];
-		var best = optimizer(a.fitness, b.fitness) ? a : b;
+		const n = population.length;
+		const a = population[Math.floor(Math.random() * n)];
+		const b = population[Math.floor(Math.random() * n)];
+		const c = population[Math.floor(Math.random() * n)];
+		let best = optimizer(a.fitness, b.fitness) ? a : b;
 		best = optimizer(best.fitness, c.fitness) ? best : c;
 		return best.genome;
 	},
@@ -19,6 +22,9 @@ export const individualSelectors = {
 		population[Math.floor(Math.random() * population.length)].genome
 };
 
+/**
+ * Parents selector for crossover operation
+ */
 export const parentsSelectors = {
 	tournament2: (population, optimizer) => [
 		individualSelectors.tournament2(population, optimizer),
